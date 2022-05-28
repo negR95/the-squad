4b79eb8419ea6032cbde73bb5280e93122d8e661
negR95 <negar.mahdavi95@gmsil.com>
2022-05-28 16:48:06 +0430

    complete project

--- script.js
+++ script.js
@@ -23,20 +23,12 @@ let data = [
         name: 'Joey',
         age: '34'
     },
-    {
-        name: 'Matero',
-        age: 30
-    },
-    {
-        name: 'Matero',
-        age: 30
-    },
 ];
 
 const info = document.querySelector('#info');
 
 let details = data.map(function(item) {
-    return '<div>' + item.name + '  ' + 'is ' + ' years old' + '</div>';
+    return '<div>' + item.name + '  ' + 'is ' + item.age + ' years old' + '</div>';
 });
 
 info.innerHTML = details.join('\n');
